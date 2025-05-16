db5f8802140f99f6e2cadb0d2fb4b9f22c29cfaf
ThanadolU <dolthanadolu@gmail.com>
2025-05-16 15:34:54 +0700

    formatted the indent of query in python files in competency questions folder

--- scripts/competency_question/similar_substance_dressing.py
+++ scripts/competency_question/similar_substance_dressing.py
@@ -22,47 +22,43 @@ def rename_uri(uri):
 
 def similar_substance():
     return """
-  PREFIX owl: <http://www.w3.org/2002/07/owl#>
-   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX s: <http://www.semanticweb.org/god/ontologies/2025/3/salad-bar-ontology#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX s: <http://www.semanticweb.org/god/ontologies/2025/3/salad-bar-ontology#>
 
-  
-   SELECT DISTINCT ?dressingX ?substancesX ?dressingY ?substancesY
-   WHERE {
-       {
-           SELECT ?saladX ?dressingX (GROUP_CONCAT(DISTINCT ?substanceX; SEPARATOR=",") AS ?substancesX)
-           WHERE {
-               ?saladX rdf:type s:Salad .
-               ?saladX s:hasDressingPortion ?dressingPortionX .
-               ?dressingPortionX s:hasDressing ?dressingX .
-               ?dressingX s:hasSubstancePortion ?substancePortionX .
-               ?substancePortionX s:hasSubstance ?substanceX .
-           }
-           GROUP BY ?saladX ?dressingX
-       }
-
-
-       {
-           SELECT ?saladY ?dressingY (GROUP_CONCAT(DISTINCT ?substanceY; SEPARATOR=",") AS ?substancesY)
-           WHERE {
-               ?saladY rdf:type s:Salad .
-               ?saladY s:hasDressingPortion ?dressingPortionY .
-               ?dressingPortionY s:hasDressing ?dressingY .
-               ?dressingY s:hasSubstancePortion ?substancePortionY .
-               ?substancePortionY s:hasSubstance ?substanceY .
-           }
-           GROUP BY ?saladY ?dressingY
-       }
-       BIND(STRAFTER(STR(?substancesX), ",") AS ?substancesX)
-       BIND(STRAFTER(STR(?substancesY), ",") AS ?substancesY)
+    SELECT DISTINCT ?dressingX ?substancesX ?dressingY ?substancesY
+    WHERE {
+        {
+            SELECT ?saladX ?dressingX (GROUP_CONCAT(DISTINCT ?substanceX; SEPARATOR=",") AS ?substancesX)
+            WHERE {
+                ?saladX rdf:type s:Salad .
+                ?saladX s:hasDressingPortion ?dressingPortionX .
+                ?dressingPortionX s:hasDressing ?dressingX .
+                ?dressingX s:hasSubstancePortion ?substancePortionX .
+                ?substancePortionX s:hasSubstance ?substanceX .
+            }
+            GROUP BY ?saladX ?dressingX
+        }
 
 
-       FILTER (?dressingX != ?dressingY && CONTAINS(?substancesY, ?substancesX))
-      
-   }
-
+        {
+            SELECT ?saladY ?dressingY (GROUP_CONCAT(DISTINCT ?substanceY; SEPARATOR=",") AS ?substancesY)
+            WHERE {
+                ?saladY rdf:type s:Salad .
+                ?saladY s:hasDressingPortion ?dressingPortionY .
+                ?dressingPortionY s:hasDressing ?dressingY .
+                ?dressingY s:hasSubstancePortion ?substancePortionY .
+                ?substancePortionY s:hasSubstance ?substanceY .
+            }
+            GROUP BY ?saladY ?dressingY
+        }
+        BIND(STRAFTER(STR(?substancesX), ",") AS ?substancesX)
+        BIND(STRAFTER(STR(?substancesY), ",") AS ?substancesY)
+        FILTER (?dressingX != ?dressingY && CONTAINS(?substancesY, ?substancesX))
+    }
     """
+
 if __name__ == "__main__":
     table = PrettyTable()
     table.field_names = [
--- scripts/competency_question/similar_substance_ingredient.py
+++ scripts/competency_question/similar_substance_ingredient.py
@@ -22,46 +22,41 @@ def rename_uri(uri):
 
 def similar_substance():
     return """
-  PREFIX owl: <http://www.w3.org/2002/07/owl#>
-   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX s: <http://www.semanticweb.org/god/ontologies/2025/3/salad-bar-ontology#>
-
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX s: <http://www.semanticweb.org/god/ontologies/2025/3/salad-bar-ontology#>
   
-   SELECT DISTINCT ?ingredientX ?substancesX ?ingredientY ?substancesY
-   WHERE {
-       {
-           SELECT ?saladX ?ingredientX (GROUP_CONCAT(DISTINCT ?substanceX; SEPARATOR=",") AS ?substancesX)
-           WHERE {
-               ?saladX rdf:type s:Salad .
-               ?saladX s:hasIngredientPortion ?ingredientPortionX .
-               ?ingredientPortionX s:hasIngredient ?ingredientX .
-               ?ingredientX s:hasSubstancePortion ?substancePortionX .
-               ?substancePortionX s:hasSubstance ?substanceX .
-           }
-           GROUP BY ?saladX ?ingredientX
-       }
+    SELECT DISTINCT ?ingredientX ?substancesX ?ingredientY ?substancesY
+    WHERE {
+        {
+            SELECT ?saladX ?ingredientX (GROUP_CONCAT(DISTINCT ?substanceX; SEPARATOR=",") AS ?substancesX)
+            WHERE {
+                ?saladX rdf:type s:Salad .
+                ?saladX s:hasIngredientPortion ?ingredientPortionX .
+                ?ingredientPortionX s:hasIngredient ?ingredientX .
+                ?ingredientX s:hasSubstancePortion ?substancePortionX .
+                ?substancePortionX s:hasSubstance ?substanceX .
+            }
+            GROUP BY ?saladX ?ingredientX
+        }
 
+        {
+            SELECT ?saladY ?ingredientY (GROUP_CONCAT(DISTINCT ?substanceY; SEPARATOR=",") AS ?substancesY)
+            WHERE {
+                ?saladY rdf:type s:Salad .
+                ?saladY s:hasIngredientPortion ?ingredientPortionY .
+                ?ingredientPortionY s:hasIngredient ?ingredientY .
+                ?ingredientY s:hasSubstancePortion ?substancePortionY .
+                ?substancePortionY s:hasSubstance ?substanceY .
+            }
+            GROUP BY ?saladY ?ingredientY
+        }
+        BIND(STRAFTER(STR(?substancesX), ",") AS ?substancesX)
+        BIND(STRAFTER(STR(?substancesY), ",") AS ?substancesY)
 
-       {
-           SELECT ?saladY ?ingredientY (GROUP_CONCAT(DISTINCT ?substanceY; SEPARATOR=",") AS ?substancesY)
-           WHERE {
-               ?saladY rdf:type s:Salad .
-               ?saladY s:hasIngredientPortion ?ingredientPortionY .
-               ?ingredientPortionY s:hasIngredient ?ingredientY .
-               ?ingredientY s:hasSubstancePortion ?substancePortionY .
-               ?substancePortionY s:hasSubstance ?substanceY .
-           }
-           GROUP BY ?saladY ?ingredientY
-       }
-       BIND(STRAFTER(STR(?substancesX), ",") AS ?substancesX)
-       BIND(STRAFTER(STR(?substancesY), ",") AS ?substancesY)
-
-
-       FILTER (?ingredientX != ?ingredientY && CONTAINS(?substancesY, ?substancesX))
-      
-   }
-
+        FILTER (?ingredientX != ?ingredientY && CONTAINS(?substancesY, ?substancesX))
+    }
     """
 if __name__ == "__main__":
     table = PrettyTable()
